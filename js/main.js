67507047f9505716c126b319e1143f84b90a84aa
Billy Purvis <purvisbilly@outlook.com>
2016-12-08 23:44:44 +0000

    initial files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// Don't forget to compile ES6
+
+// Create Vue Instance For Sign Up Form
+const signUpForm = new Vue({
+    el: '.app', 
+    data: {
+        msg: 'Vue'
+    },
+    methods: {
+
+    }
+});
