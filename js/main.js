b227512d33003eefee85bcb389ab7b138dc45ba1
Billy Purvis <purvisbilly@outlook.com>
2016-12-09 00:24:52 +0000

    Adds vue components ready for conversion

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-// register sign up form component
-Vue.component('sign-up', {
-    template: '<h1>Vue Component</h1>'
+// Create component instances
+
+// Register welcome-banner component
+Vue.component('welcome-banner', {
+template: "<h1>Welcome Banner</h1>"
+})
+
+// Register login-form component
+Vue.component('login-form', {
+template: "<h1>Login Form</h1>"
+})
+
+// Register sign-up-form component
+Vue.component('sign-up-form', {
+template: "<h1>Sign Up form</h1>"
+})
+
+// Register user-draft-card component
+Vue.component('user-draft-card', {
+template: "<h1>User Draft Card</h1>"
 })
 
 // Create root instance of Vue
